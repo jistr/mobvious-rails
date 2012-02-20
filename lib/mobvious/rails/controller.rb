89ecbc52ec6dde981c1d583a1715174caa1f099d
Jiří Stránský <jistr@jistr.com>
2012-02-20 13:05:20 +0100

    added controller module

---
+++ lib/mobvious/rails/controller.rb
@@ -0,0 +1,25 @@
+module Mobvious
+module Rails
+  module Controller
+    def device(wanted_device_type)
+      unless block_given?
+        raise ArgumentError, "Device helper takes a block of code to execute for given device."
+      end
+
+      unless device_type
+        raise "Mobvious device type not set. Did you add the Mobvious rack middleware?"
+      end
+
+      if wanted_device_type.to_sym == device_type
+        yield
+      else
+        nil
+      end
+    end
+
+    def device_type
+      request.env['mobvious.device_type']
+    end
+  end
+end
+end
---
+++ spec/mobvious/rails/controller_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+require 'mobvious/rails/controller'
+
+module Mobvious
+module Rails
+class ControllerSpec < MiniTest::Spec
+  describe Controller do
+    before do
+      @env = mock 'env'
+      @env.stubs('[]').with('mobvious.device_type').returns(:mobile)
+
+      @request = mock 'request'
+      @request.stubs(:env).returns(@env)
+      mock_request = @request
+
+      @controller = Object.new
+      @controller.extend Controller
+      @controller.define_singleton_method(:request) do
+        mock_request
+      end
+    end
+
+    it "runs the provided block only for corresponding device type" do
+      mobile_block_executed = false
+      desktop_block_executed = false
+
+      mobile_retval = @controller.device :mobile do
+        mobile_block_executed = true
+        :mobile_retval
+      end
+      mobile_block_executed.must_equal true
+      mobile_retval.must_equal :mobile_retval
+
+      desktop_retval = @controller.device :desktop do
+        desktop_block_executed = true
+        :desktop_retval
+      end
+      desktop_block_executed.must_equal false
+      desktop_retval.must_equal nil
+    end
+
+    it "returns current device type" do
+      @controller.device_type.must_equal :mobile
+    end
+  end
+end
+end
+end
