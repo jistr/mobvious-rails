b357fcc4b0d94e51b6ecf4efa0fffd203cf6b352
Jiří Stránský <jistr@jistr.com>
2012-02-24 15:00:28 +0100

    a bit clearer doc for rails engine class

--- lib/mobvious/rails/engine.rb
+++ lib/mobvious/rails/engine.rb
@@ -1,7 +1,7 @@
 module Mobvious
 module Rails
-  # Definition of this class makes it a Rails engine, enabling this library
-  # to provide assets can be used in the application.
+  # Definition of this class makes this gem a Rails engine, enabling
+  # providing assets that can be used in the application.
   class Engine < ::Rails::Engine
   end
 end
