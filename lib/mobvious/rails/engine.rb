ddc682d943ae8d10864c9a8d46798be5d83bb65d
Jiří Stránský <jistr@jistr.com>
2012-04-02 20:02:13 +0200

    assets precompilation initializer

--- lib/mobvious/rails/engine.rb
+++ lib/mobvious/rails/engine.rb
@@ -3,6 +3,11 @@ module Rails
   # Definition of this class makes this gem a Rails engine, enabling
   # providing assets that can be used in the application.
   class Engine < ::Rails::Engine
+    if Rails.version >= '3.1'
+      initializer 'mobvious-rails.assets' do |config|
+        Rails.application.config.assets.precompile += %w( mobvious-rails.js )
+      end
+    end
   end
 end
 end
